395171fbee757d1ed89c844dc3a3670fc955c72d
willings <willings.zhang@gmail.com>
2016-02-22 00:49:21 +0900

    Adapt to app engine update.

--- provider/com_appspot.go
+++ provider/com_appspot.go
@@ -3,10 +3,17 @@ package provider
 import (
 	"encoding/json"
 	"net/http"
+	"fmt"
+	"strconv"
+)
+
+const (
+	APPSPOT_URL = "http://%s.appsport.com/proxy.json?cache=%s"
 )
 
 type Com_appspot struct {
 	Appid  string
+	Cache bool
 	client *http.Client
 }
 
@@ -19,7 +26,7 @@ func (p *Com_appspot) SetClient(client *http.Client) {
 }
 
 func (p *Com_appspot) load(config interface{}) ([]*ProxyItem, error) {
-	b, err := httpGet("http://"+p.Appid +".appspot.com/refresh", p.client)
+	b, err := httpGet(fmt.Sprint(APPSPOT_URL, p.Appid, strconv.FormatBool(p.Cache)), p.client)
 	if err != nil {
 		return nil, err
 	}
