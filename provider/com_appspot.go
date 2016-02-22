ae61a8d8d0cfb33e6ae320ec02a22f2b51ee03ba
Zhang Yongjiang ( CSA3 ZHANG.Yongjiang ) <Yongjiang.Zhang@jp.sony.com>
2016-02-22 11:59:44 +0900

    Fix app engine url.

--- provider/com_appspot.go
+++ provider/com_appspot.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	APPSPOT_URL = "http://%s.appsport.com/proxy.json?cache=%s"
+	APPSPOT_URL = "http://%s.appspot.com/proxy.json?cache=%s"
 )
 
 type Com_appspot struct {
@@ -26,7 +26,8 @@ func (p *Com_appspot) SetClient(client *http.Client) {
 }
 
 func (p *Com_appspot) load(config interface{}) ([]*ProxyItem, error) {
-	b, err := httpGet(fmt.Sprint(APPSPOT_URL, p.Appid, strconv.FormatBool(p.Cache)), p.client)
+	url := fmt.Sprintf(APPSPOT_URL, p.Appid, strconv.FormatBool(p.Cache))
+	b, err := httpGet(url, p.client)
 	if err != nil {
 		return nil, err
 	}
